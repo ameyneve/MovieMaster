eb765eeafd632423ea3aed45747bafad15ceda59
Amey Neve <amey.neve@kpit.com>
2020-07-26 17:47:31 +0530

    Minor changes

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 import datetime
+import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgres://mpxagonyiygeoa:6dd6dd18c6a437c48483177f2dfcb4f45555208c70fa57552876b6b37379c537@ec2-3-216-129-140.compute-1.amazonaws.com:5432/dbkgmifuq43pbo"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'some-secret-string'
     JWT_SECRET_KEY = 'jwt-secret-string'
